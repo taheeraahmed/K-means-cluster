d29e074a3180c6bc4abe39738915fbf2a12eada0
Taheera Ahmed <taheera@hotmail.com>
2021-04-02 15:38:57 +0200

    Bug fix

---
+++ test.py
@@ -0,0 +1,201 @@
+import numpy as np
+#%matplotlib inline
+from matplotlib import pyplot as plt
+
+def plot_clusters(data, centroids):
+    """
+    Shows a scatter plot with the data points clustered according to the centroids.
+    """
+    # Assigning the data points to clusters/centroids.
+    clusters = [[] for _ in range(centroids.shape[0])]
+    for i in range(data.shape[0]):
+        distances = np.linalg.norm(data[i] - centroids, axis=1)
+        clusters[np.argmin(distances)].append(data[i])
+
+    # Plotting clusters and centroids.
+    fig, ax = plt.subplots()
+    for c in range(centroids.shape[0]):
+        if len(clusters[c]) > 0:
+            cluster = np.array(clusters[c])
+            ax.scatter(cluster[:, 0], cluster[:, 1], s=7)
+    ax.scatter(centroids[:, 0], centroids[:, 1], marker='x', s=200, c='red')
+
+    # We would like to have some control over the randomly generated data.
+# This is just for development purposes.
+np.random.seed(0)
+
+# Euclidean space.
+DIMENSIONS = 2
+
+# We will generate clusters.
+CLUSTERS = [
+    {
+        'mean': (10, 10),
+        'std': (10, 5),
+        'size': 300
+    },
+    {
+        'mean': (10, 85),
+        'std': (10, 3),
+        'size': 100
+    },
+    {
+        'mean': (50, 50),
+        'std': (6, 6),
+        'size': 200
+    },
+    {
+        'mean': (80, 75),
+        'std': (5, 10),
+        'size': 200
+    },
+    {
+        'mean': (80, 20),
+        'std': (5, 5),
+        'size': 100
+    }
+]
+
+# Initializing the dataset with zeros.
+synthetic_data = np.zeros((np.sum([c['size'] for c in CLUSTERS]), DIMENSIONS))
+
+# Generating the clusters.
+start = 0
+for c in CLUSTERS:
+    for d in range(DIMENSIONS):
+        synthetic_data[start:start + c['size'], d] = np.random.normal(c['mean'][d], c['std'][d], (c['size']))
+    start += c['size']
+print(synthetic_data)
+
+print('shape (size, dimensions) =', synthetic_data.shape)
+
+plt.figure()
+plt.scatter(synthetic_data[:, 0], synthetic_data[:, 1], s=3)
+
+def euclidean_distance(point, centroid):
+    """
+    Function implementing the euclidean distance between two points
+
+    :param point
+        np.array of a data point [x,y]
+
+    :param centroid 
+        np.array of centroid [x,y]
+    
+    :return 
+        euclidean distance between centroid and point
+    """
+    return np.sqrt(np.sum((point - centroid)**2))
+
+def calculate_centroid(clusters, centroids):
+    """
+    Function implementing the new centroids 
+
+    :param clusters
+        a list with all points in one cluster
+
+    :param centroids 
+        a numpy array of the centroids 
+
+    :return 
+        a numpy array of the updated centroids 
+    """
+    number_centroids = len(centroids)
+    #updated_centroids = np.zeros(shape=(number_centroids,2))
+    updated_centroids = []
+    
+    count = 0
+    for centroid in centroids:
+        number_points = len(clusters[count])
+        # Finding the average point given all points in one cluster
+        calculate_new_centroid = np.sum(clusters[count], axis=0)/number_points # TODO: SE PÅ DENNE
+        updated_centroids.append(calculate_new_centroid)
+        count =+ 1
+    return updated_centroids
+
+def kmeans(data, centroids):
+    """
+    Function implementing the k-means clustering.
+    
+    :param data
+        a 2D array with points
+    :param centroids
+        initial centroids
+        a 2D array containing the coordinates of initial points
+    :return
+        final centroids
+    """
+    # The key of the dictionary will be the centroids, while the items will be the points in the data
+    clusters = []
+
+    for i in range(len(centroids)): 
+        clusters.append([])
+    # For each point in data array
+    for point in data: 
+        # Find the distance between a row (point) and the centroids, key is centroid and item is distance between
+        distances = []
+        for centroid in centroids:
+            # Find the distances
+            distances.append(euclidean_distance(point, centroid))
+        # Placing a point into a cluster given the index of the minimum value
+        clusters[distances.index(min(distances))].append(point)
+    
+    # Recalculate the centroids 
+    new_centroids = calculate_centroid(clusters, centroids)
+
+    # Deciding when to stop calulating the new centroids and when not to
+    if (np.array_equal(new_centroids,centroids)):
+        print('når kommer du hit?')
+        return new_centroids
+    else:
+        kmeans(data,new_centroids)
+        return new_centroids
+
+    
+test_data = np.array([
+    [66.24345364, 57.31053969],
+    [43.88243586, 39.69929645],
+    [44.71828248, 48.38791398],
+    [39.27031378, 48.07972823],
+    [58.65407629, 55.66884721],
+    [26.98461303, 44.50054366],
+    [67.44811764, 49.13785896],
+    [42.38793099, 45.61070791],
+    [53.19039096, 50.21106873],
+    [47.50629625, 52.91407607],
+    [2.29566576, 20.15837474],
+    [18.01306597, 22.22272531],
+    [16.31113504, 20.1897911 ],
+    [13.51746037, 19.08356051],
+    [16.30599164, 20.30127708],
+    [5.21390499, 24.91134781],
+    [9.13976842, 17.17882756],
+    [3.44961396, 26.64090988],
+    [8.12478344, 36.61861524],
+    [13.71248827, 30.19430912],
+    [74.04082224, 23.0017032 ],
+    [70.56185518, 16.47750154],
+    [71.26420853, 8.57481802],
+    [83.46227301, 16.50657278],
+    [75.25403877, 17.91105767],
+    [71.81502177, 25.86623191],
+    [75.95457742, 28.38983414],
+    [85.50127568, 29.31102081],
+    [75.60079476, 22.85587325],
+    [78.08601555, 28.85141164]
+])
+
+test_centroids = np.array([
+    [25, 50],
+    [50, 50],
+    [75, 50]
+])
+
+test_centroids = kmeans(test_data, test_centroids)
+
+print('c0 =', test_centroids[0])
+print('c1 =', test_centroids[1])
+print('c2 =', test_centroids[2])
+plot_clusters(test_data, test_centroids)
+    
+    
